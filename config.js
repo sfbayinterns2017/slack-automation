59e6195215a21c1b6ac2beebf9b51376a51c448e
aanant <aanant@umich.edu>
2018-02-27 19:36:37 -0800

    changed config

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    community: process.env.COMMUNITY_NAME || 'SF Bay Interns 2017',
-    slackUrl: process.env.SLACK_URL || 'sfbayinterns2017.slack.com',
-    slacktoken: process.env.SLACK_TOKEN || 'YOUR SLACK TOKEN',
+    community: process.env.COMMUNITY_NAME || 'SF Bay Interns 2018',
+    slackUrl: process.env.SLACK_URL || 'sfbayinterns2018.slack.com',
+    slacktoken: process.env.SLACK_TOKEN || 'xoxp-233911432112-234620986004-321967653844-0dff959752742af29d8e802ae19ba3e6',
     locale: process.env.LOCALE || 'en',
-    airtableKey: process.env.AIRTABLE_API_KEY || 'YOUR AIRTABLE API KEY',
+    airtableKey: process.env.AIRTABLE_API_KEY || 'keybNudaRbNnNyLdN',
 };
